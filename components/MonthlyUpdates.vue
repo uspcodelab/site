cdf75c161d42aa9942bb64a16feb9b0504d2fad5
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-05-22 22:50:15 -0300

    Create Monthly Updates page and component
    Create a new `updates.vue` page, to create a new `/updates` route.
    
    Create new componet `MonthlyUpdates.vue` to be the container each post,
    or contain the posts themselves.
    
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

---
+++ components/MonthlyUpdates.vue
@@ -0,0 +1,82 @@
+<template>
+  <b-row id="dev-journey" tag="section" class="dev-journey px-3 py-5 px-sm-5">
+    <b-col sm="12" class="py-5">
+      <h1 class="text-primary text-center pb-3">
+        Monthly Updates
+      </h1>
+      <p class="text-center">
+        Saiba o que o CodeLab anda fazendo!
+      </p>
+    </b-col>
+    <b-col sm="12" class="dev-journey__info py-3 p-md-2">
+      <b-card body-bg-variant="light" class="text-center">
+        <h3 class="dev-journey-phase">Junho 2018</h3>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Nemo rem quidem asperiores nisi accusamus, qui mollitia nostrum 
+        atque ex, velit temporibus blanditiis deleniti molestiae! 
+        Iusto fuga omnis esse ut dignissimos.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Nemo rem quidem asperiores nisi accusamus, qui mollitia nostrum 
+        atque ex, velit temporibus blanditiis deleniti molestiae! 
+        Iusto fuga omnis esse ut dignissimos.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Nemo rem quidem asperiores nisi accusamus, qui mollitia nostrum 
+        atque ex, velit temporibus blanditiis deleniti molestiae! 
+        Iusto fuga omnis esse ut dignissimos.
+      </b-card>
+    </b-col>
+    <b-col sm="12" class="dev-journey__info py-3 p-md-2">
+      <b-card body-bg-variant="light" class="text-center">
+        <h3 class="dev-journey-phase">Maio 2018</h3>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Nemo rem quidem asperiores nisi accusamus, qui mollitia nostrum 
+        atque ex, velit temporibus blanditiis deleniti molestiae! 
+        Iusto fuga omnis esse ut dignissimos.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Nemo rem quidem asperiores nisi accusamus, qui mollitia nostrum 
+        atque ex, velit temporibus blanditiis deleniti molestiae! 
+        Iusto fuga omnis esse ut dignissimos.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Nemo rem quidem asperiores nisi accusamus, qui mollitia nostrum 
+        atque ex, velit temporibus blanditiis deleniti molestiae! 
+        Iusto fuga omnis esse ut dignissimos.
+      </b-card>
+    </b-col>
+    <b-col sm="12" class="dev-journey__info py-3 p-md-2">
+      <b-card body-bg-variant="light" class="text-center">
+        <h3 class="dev-journey-phase">Abril 2018</h3>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Nemo rem quidem asperiores nisi accusamus, qui mollitia nostrum 
+        atque ex, velit temporibus blanditiis deleniti molestiae! 
+        Iusto fuga omnis esse ut dignissimos.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Nemo rem quidem asperiores nisi accusamus, qui mollitia nostrum 
+        atque ex, velit temporibus blanditiis deleniti molestiae! 
+        Iusto fuga omnis esse ut dignissimos.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Nemo rem quidem asperiores nisi accusamus, qui mollitia nostrum 
+        atque ex, velit temporibus blanditiis deleniti molestiae! 
+        Iusto fuga omnis esse ut dignissimos.
+      </b-card>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.dev-journey {
+  color: theme-color("dark");
+  background-color: theme-color("light");
+
+  &__info {
+    display: inline-flex;
+
+    &__icon {
+      max-width: 80%;
+    }
+  }
+}
+</style>
---
+++ pages/updates.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="page">
+    <the-navbar/>
+    <b-container fluid>
+      <main>
+        <MonthlyUpdates />
+      </main>
+      <the-footer/>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import TheNavbar from "~/components/TheNavbar.vue";
+import TheFooter from "~/components/TheFooter.vue";
+import MonthlyUpdates from "~/components/MonthlyUpdates.vue";
+
+export default {
+  components: {
+    TheNavbar,
+    TheFooter,
+    MonthlyUpdates
+  }
+};
+</script>
+
+<style lang="scss">
+html {
+  font-size: 14px;
+}
+
+.page {
+  padding-top: 4rem;
+}
+
+@include media-breakpoint-up(lg) {
+  html {
+    font-size: 16px;
+  }
+}
+
+@include media-breakpoint-up(xl) {
+  html {
+    font-size: 1.5vmax;
+  }
+}
+</style>
